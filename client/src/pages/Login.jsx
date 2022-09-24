dd8a774104fe842b49196c5b4d3f42a2d6ceb394
sebastianiv21 <seluti98@gmail.com>
2022-09-23 23:08:23 -0500

    Estilizada vista de login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,25 @@
-
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <main>
+      <article className="container">
+        <div className="flex">
+          <form className="bg-base rounded-md mx-auto my-20 p-3 text-oscuro text-lg">
+            <div className="flex flex-col text-center my-1 mx-5">
+              <label htmlFor="username">Usuario</label>
+              <input type="text" id="username" className="text-oscuro" />
+            </div>
+            <div className="flex flex-col text-center my-1 mx-5">
+              <label htmlFor="password">Contraseña</label>
+              <input type="password" id="password" className="text-oscuro" />
+            </div>
+            <div className="text-center text-white my-1 mx-5">
+              <button className="bg-oscuro rounded-md py-1 px-2 text-sm mt-3">Iniciar sesión</button>
+            </div>
+          </form>
+        </div>
+      </article>
+    </main>
+  );
+};
 
-export default Login
+export default Login;
